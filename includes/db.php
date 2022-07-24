b71df3aa6656862df2d0efe8755c68016eb73569
Łukasz Sobel <lukas1474@gmail.com>
2022-07-24 15:37:06 +0200

    connect main view with database

--- includes/db.php
+++ includes/db.php
@@ -10,7 +10,7 @@
 
   $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-  if($connection) {
-    echo 'connected';
-  }
+  // if($connection) {
+  //   echo 'connected';
+  // }
 ?>
