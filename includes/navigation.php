6e53d4273b065282f950d2b6e44003540a4764a0
Łukasz Sobel <lukas1474@gmail.com>
2022-07-26 20:42:34 +0200

    prepare admin view

--- includes/navigation.php
+++ includes/navigation.php
@@ -23,6 +23,9 @@
             echo "<li><a href='#'>{$cat_title}</a></li>";
           }
         ?>
+        <li>
+          <a href="admin">Admin</a>
+        </li>
       </ul>
     </div>
     <!-- /.navbar-collapse -->
