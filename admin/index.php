8e7aab6bae5809e842d0d694df7c4b9007e9f079
Łukasz Sobel <lukas1474@gmail.com>
2022-07-30 20:27:36 +0200

    add login for users

--- admin/index.php
+++ admin/index.php
@@ -1,24 +1,17 @@
 <?php include "includes/AdminHeader.php" ?>
 
 <div id="wrapper">
-  <!-- Navigation -->
   <?php include "includes/AdminNavigation.php" ?>
-
   <div id="page-wrapper">
     <div class="container-fluid">
-      <!-- Page Heading -->
       <div class="row">
         <div class="col-lg-12">
-          <h1 class="page-header">
-            Dzień dobry :)
-            <small>widok główny</small>
-          </h1>
+          <h2 class="page-header">
+            Witaj w Imperium
+            <small><?php echo $_SESSION['username'] ?></small>
+          </h2>
         </div>
       </div>
-      <!-- /.row -->
     </div>
-    <!-- /.container-fluid -->
   </div>
-  <!-- /#page-wrapper -->
-
 <?php include "includes/AdminFooter.php" ?>    
--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,6 +1,18 @@
 <div class="col-md-4">
- 
-  <!-- Blog Search Well -->
+   <div class="well">
+    <h4>Logowanie</h4>
+    <form action="includes/login.php" method="post">
+      <div class="form-group">
+        <input name="username" type="text" class="form-control" placeholder="Nazwa użytkownika" >
+      </div>
+      <div class="input-group">
+        <input name="password" type="password" class="form-control" placeholder="Hasło">
+        <span class="input-group-btn">
+          <button class="btn btn-primary" name="login" type="submit">Zaloguj</button>
+        </span>
+      </div>
+    </form>
+  </div>
   <div class="well">
     <h4>Blog Search</h4>
     <form action="search.php" method="post">
@@ -13,10 +25,7 @@
         </span>
       </div>
     </form>
-    <!-- /.input-group -->
   </div>
-
-  <!-- Blog Categories Well -->
   <div class="well">
     <?php 
       $query = "SELECT * FROM categories";
@@ -36,10 +45,7 @@
           ?>
         </ul>
       </div>
-        <!-- /.col-lg-6 -->
     </div>
-    <!-- /.row -->
   </div>
-  <!-- Side Widget Well -->
   <?php include "widget.php" ?>
 </div>
